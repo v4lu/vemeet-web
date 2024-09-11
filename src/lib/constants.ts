0857c0d1381e32245f9f2adb7d1432be65647554
Luka <lukabrkovic@proton.me>
2024-09-11 12:56:27 +0200

    devops: fixed dockerfile

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@ export const SERVER_BASE_URL = 'http://core-app:8070/v1';
 export const ACCESS_TOKEN = 'access_token';
 export const REFRESH_TOKEN = 'refresh_token';
 export const COGNITO_ID = 'cognito_id';
-export const isProduction = false;
+export const isProduction = true;
