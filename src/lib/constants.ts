aef059e48440fcefbd59ddb46cbdb824ee96d2de
Luka <lukabrkovic@proton.me>
2024-09-12 22:53:32 +0200

    fix: constant value

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const CLIENT_BASE_URL = 'https://api.vemeet.me/v1';
-export const SERVER_BASE_URL = 'https://api.vemeet.me/v1';
+export const SERVER_BASE_URL = 'http://core-app:8070/v1';
 export const ACCESS_TOKEN = 'access_token';
 export const REFRESH_TOKEN = 'refresh_token';
 export const COGNITO_ID = 'cognito_id';
