8e4704199ab919e73ea6ff27f7721ff0bc6fe0f9
Luka <lukabrkovic@proton.me>
2024-09-14 14:04:53 +0200

    refactor: image converter service used for image upload

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -72,7 +72,7 @@ export async function uploadImage({
 		formData.append('file', file);
 		try {
 			const res = await api
-				.post<{ url: string }>('files/single', {
+				.post<{ url: string }>('files/images-avif', {
 					headers: {
 						Authorization: `Bearer ${authToken}`
 					},
